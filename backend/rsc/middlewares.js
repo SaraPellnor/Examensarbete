75a9ffac4e3d2e8e50be69a5c27f6776f490dad3
SaraPellnor <sara.ahlquist@hotmail.com>
2024-01-03 20:06:09 +0100

    buggfix and error handling, comment code

--- backend/rsc/middlewares.js
+++ backend/rsc/middlewares.js
@@ -1,15 +1,26 @@
-// const {userValidation} = require("./user/user.model")
+// ---- Middleware function for general validation based on a Joi schema
+
 const validation = (joiSchema) => {
   return (req, res, next) => {
     try {
-      console.log(req.body, joiSchema);
+      // Validate incoming data against the specified Joi schema
       const validated = joiSchema.validate(req.body);
-      validated && next();
+
+      // If validation fails, send a 400 Bad Request status with the error message
+      validated.error
+        ? res.status(400).json(validated.error.message)
+        : // If validation is successful, proceed to the next middleware
+          next();
     } catch (err) {
+      // If validation encounters an error, send a 400 Bad Request status with the error
       res.status(400).json(err);
     }
   };
 };
+
+
+
+// ---- Export the validation function for use in other parts of the application
 
 module.exports = {
   validation,
