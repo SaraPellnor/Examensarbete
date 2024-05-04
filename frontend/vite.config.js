5e25713949cfefefe7a7d3a8b08e63ec86a35e1a
SaraPellnor <sara.ahlquist@hotmail.com>
2024-05-04 19:28:16 +0200

    "builds": [     { "src": "frontend/package.json", "use": "@vercel/static-build" }   ]

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  builds: [{ src: "frontend/package.json", use: "@vercel/static-build" }],
   build: {
     outDir: "dist", // Den här mappen innehåller den kompilerade koden
   },
