158f19cc1ca098a1d5d3cdd02a1df0be8498a8e0
SaraPellnor <sara.ahlquist@hotmail.com>
2024-05-04 17:52:46 +0200

    build: {     outDir: 'dist', // Den här mappen innehåller den kompilerade koden   },

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist", // Den här mappen innehåller den kompilerade koden
+  },
   plugins: [react()],
-    server: {
+  server: {
     proxy: {
       "/app": {
         target: "http://localhost:3000",
       },
     },
   },
-})
+});
